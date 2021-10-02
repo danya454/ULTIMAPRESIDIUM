0db06b62c2d4288e007e3b930f2344b143791295
daniil <daniil@AirDandy-aka-Lollipop.local>
2021-10-02 10:12:40 +0300

    def add(x, y)

--- git.py
+++ git.py
@@ -1,4 +1,9 @@
 import re
 f=open('/Library/Frameworks/Python.framework/Versions/3.8/Resources/English.lproj/Documentation/_sources/whatsnew/2.4.rst.txt')
 print(f.read())
-print(1+2) 
+print(1+2)
+def add(x, y):
+    return x+y
+x=int(input())
+y=int(input())
+print(add(x,y))
